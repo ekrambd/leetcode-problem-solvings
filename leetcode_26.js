1905a14725769597d48b36a17be6d384d46ff867
ekrmabd <ekramhossainekram28@gmail.com>
2025-12-22 17:12:54 +0600

    rewrite leetcode 26

--- leetcode_26.js
+++ leetcode_26.js
@@ -1,15 +1,18 @@
 //26. Remove Duplicates from Sorted Array
 
 var removeDuplicates = function(nums) {
-    let arr = Array.from(new Set(nums));
-    for(let i = 0; i < arr.length; i++)
-    {
-        nums[i] = arr[i];
+    
+    let unique = Array.from(new Set(nums));
+
+    for (let i = 0; i < unique.length; i++) {
+        nums[i] = unique[i];
     }
-    return nums.length
+    
+    return unique.length; 
 };
-//console.log(removeDuplicates([1,1,2]));
+
+
 let arr = [1,1,2];
 let len = removeDuplicates(arr);
-let result = arr.slice(0,len);
-return result;
+console.log(len); 
+console.log(arr.slice(0,len));
---
+++ leetcode_7.js
@@ -0,0 +1,6 @@
+//7. Reverse Integer
+
+var reverse = function(x) {
+    return x;
+};
+console.log(reverse(123));
