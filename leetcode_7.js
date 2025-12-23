97de095af4ed15b71780c872a4fa123dfb2c3cdc
ekrmabd <ekramhossainekram28@gmail.com>
2025-12-23 10:26:02 +0600

    leetcode 7 reverse integer

--- leetcode_7.js
+++ leetcode_7.js
@@ -1,6 +1,33 @@
 //7. Reverse Integer
 
 var reverse = function(x) {
-    return x;
+    let status = true;
+    if (x < 0) {
+        x = Math.abs(x);
+        status = false;
+    }
+
+    let arr = String(x).split("");
+    let left = 0, right = arr.length - 1;
+
+    while (left < right) {
+        [arr[left], arr[right]] = [arr[right], arr[left]];
+        left++;
+        right--;
+    }
+
+    let result = Number(arr.join(""));
+    if (!status) result *= -1;
+
+    // 🚨 overflow check
+    if (result < -(2 ** 31) || result > (2 ** 31 - 1)) {
+        return 0;
+    }
+
+    return result;
 };
+
 console.log(reverse(123));
+console.log(reverse(120));
+console.log(reverse(-123));
+console.log(reverse(1534236469)); 
